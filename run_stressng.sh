9cc9f798b6fd346f3ccc0269ed6ae18a05bfa5b0
paulasanematsu <paula_sanematsu@g.harvard.edu>
2025-09-24 17:59:14 -0400

    Change output file, add more comments

--- run_stressng.sh
+++ run_stressng.sh
@@ -3,11 +3,12 @@
 # calculate number of cores for matrix stressor
 # high- and low-usage cpu cores are listed at the top of cpu.job file
 # this is based on: N_matrix= (total cores) - (high-usage cpu cores) - 0.75*(low-cpu usage cores)
-#                           =$(nproc --all) -           5            -            10
-let "N_matrix="$(nproc --all)"-15"
+#                           =$(nproc --all) -           5            - 0.75*(        13         )
+let "N_matrix="$(nproc --all)"-13"
+#let "N_matrix="$(nproc --all)-20""
 
 # stress-ng log file
-output_file=/n/netscratch/rc_admin/Lab/stress-test/"$(hostname -s)"-"$(date "+%Y-%m-%d")".txt
+output_file=/odyssey/paulasan/stress-test/cpu_output/"$(hostname -s)"-"$(date "+%Y-%m-%d-%H-%M-%S")".txt
 
 # run stress-ng
 stress-ng --matrix $N_matrix --job cpu.job --log-file $output_file 

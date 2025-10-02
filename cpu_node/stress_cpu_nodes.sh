07ceeb2941634fb9948ad7e2c4f8e0fc84b2db22
paulasanematsu <paula_sanematsu@g.harvard.edu>
2025-10-02 15:52:24 -0400

    Organize help output

--- cpu_node/stress_cpu_nodes.sh
+++ cpu_node/stress_cpu_nodes.sh
@@ -4,22 +4,25 @@ helpstr="\
 stress_cpu_node.sh gathers node information and submits a slurm job stress-ng 
 to stress a cpu node (memory, cpu, and kernel)
 
-Usage: ./stress_cpu_nodes.sh -f <list_of_nodes> -t <time_in_minutes> -y [-h for help]
+Usage: ./stress_cpu_nodes.sh -f <list_of_nodes> [-r <reservation_name>] -t <time_in_minutes> -y [-h for help]
 
   flag    argument          comment
     -f    list_of_nodes     text file containing list of nodes to stress
                             one node name per line
+    -r    reservation       (optional) reservation name
     -t    time_in_minutes   time to run stress test, in minutes
     -y                      add -y to actually submit the job
                             without -y is a dry run
 "
 
-while getopts "f:ht:y" opt; do
+while getopts "f:hr:t:y" opt; do
   case $opt in
 	f) node_list_file="$OPTARG" 
 	;;
 	h) echo -n "$helpstr" 
 	   exit 0
+	;;
+	r) reservation="$OPTARG"
 	;;
 	t) run_time="$OPTARG"
 	;;
@@ -38,7 +41,19 @@ done
 echo "========= All nodes summary =========="
 echo "run_time          ${run_time} minutes"
 echo "node_list_file    ${node_list_file}"
-echo "submit job        ${submit_job}"
+
+if [ "${submit_job}" = true ] ; then
+    echo "submit job        ${submit_job}"
+else
+    echo "submit job        false"
+fi
+
+if [ -z "${reservation+set}" ]; then
+    echo "reservation       (empty)"
+else
+    echo "reservation       ${reservation}"
+    sbatch_args="--reservation ${reservation}"
+fi
 
 while read nodename; do
     # get primary partition that node belongs to
@@ -62,12 +77,16 @@ while read nodename; do
     echo "    job_partition     ${job_partition}"
     echo "    slurm_mem         ${slurm_mem}"
     echo "    total_cpus        ${total_cpus}"
-    
+
+    sbatch_args="${sbatch_args} --time=${run_time} --partition ${job_partition} --mem=${slurm_mem} -c ${total_cpus} --nodelist ${nodename}"
+
     # submit cpu job
     if [ "${submit_job}" = true ] ; then
-        sbatch --time=${run_time} --partition ${job_partition} --mem=${slurm_mem} -c ${total_cpus} --nodelist ${nodename} slurm_stressng_job.sh
+        sbatch ${sbatch_args} slurm_stressng_job.sh
     else
-	echo "    Dry run. Add -y to ./stress_cpu_nodes.sh to run"
-	echo "    sbatch --time=${run_time} --partition ${job_partition} --mem=${slurm_mem} -c ${total_cpus} --nodelist ${nodename} slurm_stressng_job.sh"
+        echo " "
+        echo "    **Dry run.**"
+        echo "    Add -y to ./stress_gpu_nodes.sh to run. Commands that would be executed:"
+        echo "        sbatch ${sbatch_args} slurm_stressng_job.sh"
     fi
 done < ${node_list_file}

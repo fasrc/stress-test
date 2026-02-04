2786587608374fb736e2d8d628d81871015ff6ee
paulasanematsu <paula_sanematsu@g.harvard.edu>
2026-02-04 15:55:06 -0500

    Add --detail argument and detailed summary

--- postprocessing/summary.py
+++ postprocessing/summary.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import argparse
-from datetime import datetime
+import datetime
 import re
 
 def printf(format, *args):
@@ -15,9 +15,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--date", dest="run_date",
                     required=True,
                     help="date in YYYY-MM-DD that the stress test started")
+parser.add_argument("-D", "--detail",
+                    required=False,
+                    action='store_true',
+                    help="output more details (e.g. path of log files)")
 parser.add_argument("-g", "--debug", dest="debug",
-                    required=False, 
-                    default=False,
+                    required=False,
+                    action='store_true',
                     help="debug mode (add print statements)")
 parser.add_argument("-n", "--node-type", dest="node_type",
                     choices=["cpu", "gpu"],
@@ -29,6 +33,7 @@ args = parser.parse_args()
 run_date = args.run_date
 node_type = args.node_type
 debug = args.debug
+detail = args.detail
 
 # Enforce strict YYYY-MM-DD format: 4-2-2 digits
 if not re.match(r"^\d{4}-\d{2}-\d{2}$", run_date):
@@ -36,7 +41,7 @@ if not re.match(r"^\d{4}-\d{2}-\d{2}$", run_date):
 
 # Also validate that it's a real date
 try:
-    datetime.strptime(run_date, "%Y-%m-%d")
+    datetime.datetime.strptime(run_date, "%Y-%m-%d")
 except ValueError:
     parser.error("Argument -d/--date must be a valid date in YYYY-MM-DD format")    
 
@@ -57,6 +62,7 @@ print("")
 node_list = []
 jobID_list = []
 stressng_status_list = []
+filename_list = []
 if node_type=="gpu":
     gpuburn_status = []
 
@@ -69,6 +75,7 @@ if node_type=="gpu":
 for filename in os.listdir(folder_path):
     if filename.endswith(".out"):
         file_path = os.path.join(folder_path, filename)
+        filename_list.append(filename)
         if debug: print("    File: " + filename)
         with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
 
@@ -88,17 +95,33 @@ for filename in os.listdir(folder_path):
                     stressng_status_list.append("failed")
                 # a successful run
                 elif "failed: 0" in all_text.lower():
-                    #for lineno, line in enumerate(f, start=1):
-                    #    if "fail:" in line.lower():
-                    #        # rstrip() to remove trailing newline
-                    #        # print(f"'failed' found in: {filename} (line {lineno}): {line.rstrip()}")
-                    #        print("        'fail' found in line " + str(lineno) + ": " + line)
                     if debug: print("        Inside successful " + jobID)
                     stressng_status_list.append("success")
             # run did not finish
             else:
                 if debug: print("        Inside incomplete " + jobID)
                 stressng_status_list.append("incomplete run")
+
+            if detail:
+                print("Inside detail" + filename)
+                for lineno, line in enumerate(f, start=1):
+                    print(line)
+                    if "stress-ng run time" in line.lower():
+                        # rstrip() to remove trailing newline
+                        # print(f"'failed' found in: {filename} (line {lineno}): {line.rstrip()}")
+                        print("        'run' found in line " + str(lineno) + ": " + line)
+                for i, line in enumerate(all_text.splitlines(), 1):
+                    # get requested run time
+                    if "stress-ng run time" in line:
+                        print(f"Found 'stress-ng run time' in {filename} at line {i}")
+                        print(line)
+                        run_time_seconds = float(line.split(" ")[-2])
+                        run_time_formatted = datetime.timedelta(seconds=run_time_seconds)
+                        print(run_time_formatted)
+
+                # get final run time
+                if stressng_status_list[-1] != "incomplete run":
+                    print(line)
 
 if debug: print("")
 
@@ -114,14 +137,26 @@ if debug: print("")
 
 # cpu nodes
 if node_type=="cpu":
-    printf("-----------------------------------------------\n")
-    printf("           Node     job ID    stress-ng status \n")
-    printf("-----------------------------------------------\n")
+    # short summary
+    if not detail:
+        printf("-----------------------------------------------\n")
+        printf("           Node     job ID    stress-ng status \n")
+        printf("-----------------------------------------------\n")
 
-    for i in range(len(node_list)):
-        printf("%15s %10s %19s\n", node_list[i], jobID_list[i], stressng_status_list[i])
+        for i in range(len(node_list)):
+            printf("%15s %10s %19s\n", node_list[i], jobID_list[i], stressng_status_list[i])
 
-    printf("-----------------------------------------------\n")
+        printf("-----------------------------------------------\n")
+    # detailed summary
+    else:
+        printf("--------------------------------------------------------------------------------------------------------------------------------------------------\n")
+        printf("           Node     job ID    stress-ng status logfile (root dir above)\n")
+        printf("--------------------------------------------------------------------------------------------------------------------------------------------------\n")
+
+        for i in range(len(node_list)):
+            printf("%15s %10s %19s %s\n", node_list[i], jobID_list[i], stressng_status_list[i], filename_list[i])
+
+        printf("--------------------------------------------------------------------------------------------------------------------------------------------------\n")
 
 # gpu nodes
 if node_type=="gpu":
